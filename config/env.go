1841c12a3ad1d14e1a664c8af9fa3c210192be76
Zeni <muhammadzainigunanda@gmail.com>
2026-05-27 01:33:11 +0700

    fix:dont fatal when .env file not found

--- config/env.go
+++ config/env.go
@@ -8,6 +8,9 @@ import (
 
 func LoadEnv() {
 	if err := godotenv.Load(); err != nil {
+		// ganti log.Fatal() ke log.Println() biar
+		// nggak crash waktu saat file .env kagak ada
+		// waktu docker build
 		log.Println("No .env file found, using injected environment variables")
 	}
 }
